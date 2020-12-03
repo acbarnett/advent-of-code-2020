63b398eec2f013b1d1578b4695006cfff94a9c0c
Aaron Barnett <aaron.barnett@outlook.com>
2020-12-02 18:56:18 -0800

    Style changes to 01

--- 01/1a.py
+++ 01/1a.py
@@ -1,6 +1,5 @@
-with open('input-a.txt','r') as f:
-# with open('input-a-demo.txt','r') as f:
-  all_entries = [int(line.rstrip()) for line in f]
+with open("input-a.txt", "r") as f:
+  all_entries = [int(line.strip()) for line in f]
 
 for i in range(0, len(all_entries)):
   for j in range(i+1, len(all_entries)):
--- 01/1b.py
+++ 01/1b.py
@@ -1,4 +1,4 @@
-with open('input-a.txt','r') as f:
+with open("input-a.txt", "r") as f:
   all_entries = [int(line.rstrip()) for line in f]
 
 for i in range(0, len(all_entries)):
