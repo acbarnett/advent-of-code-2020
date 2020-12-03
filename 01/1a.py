f4468d07906423d7d7a82b9049bd817d31a19981
Aaron Barnett <aaron.barnett@outlook.com>
2020-12-02 18:19:23 -0800

    Python scripts to solve problem 1, input file fixed.

---
+++ 01/1a.py
@@ -0,0 +1,8 @@
+with open('input-a.txt','r') as f:
+# with open('input-a-demo.txt','r') as f:
+  all_entries = [int(line.rstrip()) for line in f]
+
+for i in range(0, len(all_entries)):
+  for j in range(i+1, len(all_entries)):
+    if all_entries[i] + all_entries[j] == 2020:
+      print(all_entries[i] * all_entries[j])
---
+++ 01/1b.py
@@ -0,0 +1,10 @@
+with open('input-a.txt','r') as f:
+  all_entries = [int(line.rstrip()) for line in f]
+
+for i in range(0, len(all_entries)):
+  for j in range(i+1, len(all_entries)):
+    if all_entries[i] + all_entries[j] > 2019:
+      continue
+    for k in range(j+1, len(all_entries)):
+      if all_entries[i] + all_entries[j] + all_entries[k] == 2020:
+        print(all_entries[i] * all_entries[j] * all_entries[k])
