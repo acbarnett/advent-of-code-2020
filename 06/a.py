de6a4a35989dc0a80139ba045a7ebd344d42a9e6
Aaron Barnett <aaron.barnett@outlook.com>
2020-12-05 21:46:27 -0800

    [06] Solution to part a

---
+++ 06/a.py
@@ -0,0 +1,25 @@
+name = "a-input.txt"
+
+def get_groups(name):
+  groups = []
+  with open(name, "r") as f:
+    unbroken = f.read()
+    groups = [s.strip() for s in unbroken.split("\n\n")]
+
+  return groups
+
+def get_questions_per_group(group):
+  one_line = group.replace("\n", "")
+  characters = [c for c in one_line]
+  return set(characters)
+
+groups = get_groups(name)
+
+sum = 0
+for group in groups:
+  print(group)
+  uniques = get_questions_per_group(group)
+  sum = sum + len(uniques)
+
+print("Sum of unique questions per group")
+print(sum)
