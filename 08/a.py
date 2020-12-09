07376840ed1d22f0bb874136d245a8bd552de82d
Aaron Barnett <aaron.barnett@outlook.com>
2020-12-08 16:37:22 -0800

    [08] Move things into named constants

--- 08/a.py
+++ 08/a.py
@@ -8,35 +8,43 @@ def get_lines(name):
 
   return lines
 
+NOP = "nop"
+ACC = "acc"
+JMP = "jmp"
+
+ACC_VALUE = "ACC_VALUE"
+CUR_LINE = "CUR_LINE"
+LINES_EXECUTED = "LINES_EXECUTED"
+
 
 program_state = {
-  "acc": 0,
-  "cur_line": 0,
-  "lines_executed": []
+  ACC_VALUE: 0,
+  CUR_LINE: 0,
+  LINES_EXECUTED: []
 }
 
 def execute_a(state, lines):
-  line = lines[state["cur_line"]]
+  line = lines[state[CUR_LINE]]
   op, instruction = line.split(" ")
   instruction = int(instruction)
 
-  if state["cur_line"] in state["lines_executed"]:
-    return state["acc"]
+  if state[CUR_LINE] in state[LINES_EXECUTED]:
+    return state[ACC_VALUE]
 
-  if op == "nop":
-    state["lines_executed"].append(state["cur_line"])
-    state["cur_line"] += 1
+  if op == NOP:
+    state[LINES_EXECUTED].append(state[CUR_LINE])
+    state[CUR_LINE] += 1
     return execute_a(state, lines)
 
-  if op == "acc":
-    state["lines_executed"].append(state["cur_line"])
-    state["cur_line"] += 1
-    state["acc"] += instruction
+  if op == ACC:
+    state[LINES_EXECUTED].append(state[CUR_LINE])
+    state[CUR_LINE] += 1
+    state[ACC_VALUE] += instruction
     return execute_a(state, lines)
 
-  if op == "jmp":
-    state["lines_executed"].append(state["cur_line"])
-    state["cur_line"] += instruction
+  if op == JMP:
+    state[LINES_EXECUTED].append(state[CUR_LINE])
+    state[CUR_LINE] += instruction
     return execute_a(state, lines)
 
 
