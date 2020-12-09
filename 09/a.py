58011373e9f627f8010f07658c0d1dcc2c0af885
Aaron Barnett <aaron.barnett@outlook.com>
2020-12-08 22:54:58 -0800

    [09] Solution to part b, done inline instead of in a function to save on data swapping

--- 09/a.py
+++ 09/a.py
@@ -12,27 +12,32 @@ def get_nums(name):
   return nums
 
 def is_sum_of_prev(num_to_check, possible_components):
-  # print("Checking")
-  # print(num_to_check)
-  # print([num_to_check, possible_components])
   for i in range(0, len(possible_components)):
     for j in range(i + 1, len(possible_components)):
-      print([possible_components[i], possible_components[j], num_to_check])
       if possible_components[i] + possible_components[j] == num_to_check:
         return True
-      # print([possible_components[i], possible_components[j]])
   return False
 
 
-
-
 numbers = get_nums(filename)
-
-# print(numbers)
-
 
 print("Solution to part a")
 for i in range(preamble_length, len(numbers)):
   if not is_sum_of_prev(numbers[i], numbers[i - preamble_length:i]):
     print(numbers[i])
+    part_a_solution = numbers[i]
+    break
+
+print("Solution to part b")
+for i in range(0, len(numbers)):
+  match_found = False
+  sum = numbers[i]
+  for j in range(i+1, len(numbers)):
+    sum += numbers[j]
+    if sum == part_a_solution:
+      valid_range = numbers[i:j+1]
+      print(min(valid_range) + max(valid_range))
+      match_found = True
+      break
+  if match_found:
     break
